b8fb00a6921279a484d7e314a2c01cedeb7e29d0
Eduardo-Amaral <eduardo.lopes.amaral@outlook.com>
2022-02-13 13:12:27 -0300

    transition added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter,
-  Routes,
-  Route
+  
+  Route,
+  useLocation,
+  Routes
 } from "react-router-dom";
-import Projects from 'pages/Projects';
+//@ts-ignore
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Home from "pages/Home";
-import Navbar from "components/Navbar";
-import Contacts from "pages/Contacts";
+import Projects from "pages/Projects";
 import KnowHow from "pages/KnowHow";
+import Contacts from "pages/Contacts";
 
-function App() {
+import "./index.css";
+//@ts-ignore
+import styled from "styled-components";
+import Navbar from "components/Navbar";
+
+const Main = styled.main``;
+
+const AnimatedSwitch = () => {
+  const location = useLocation();
+
+  console.log("location", location);
+
+  const [transitionName] = useState("next");
+
+  /*
+  useEffect(() => {
+    if (transitionName === "next") setTransitionName("prev");
+    if (transitionName === "prev") setTransitionName("next");
+  }, [location]);
+  */
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Projetos">
-          <Route path="" element={<Projects />} />
-        </Route>
-        <Route path="/Conhecimentos">
-          <Route path="" element={<KnowHow />} />
-        </Route>
-        <Route path="/Contatos">
-          <Route path="" element={<Contacts />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <TransitionGroup component={Main}>
+      <CSSTransition
+        key={location.pathname}
+        classNames={transitionName}
+        timeout={500}
+      >
+        <Routes location={location}>
+        {/* @ts-ignore */}
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projetos" element={<Projects />} />
+          <Route path="/conhecimentos" element={<KnowHow />} />
+          <Route path="/contatos" element={<Contacts />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+        <AnimatedSwitch />
+      </BrowserRouter>
+    </div>
   );
 }
-
-export default App;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,22 +2,27 @@ import BurguerMenu from 'components/BurguerMenu';
 import { Link } from 'react-router-dom';
 import './index.css';
 function Navbar() {
+
   return (
     <>
       <header >
         <nav className='container'>
           <div className='mywebsite-nav-content'>
           <h1 id="logo">EA</h1>
+          {/* @ts-ignore */}
             <Link className="links" to={`/`}>
               <p >Início</p>
             </Link>
-            <Link className="links" to={`/Projetos/`}>
+              {/* @ts-ignore */}
+            <Link className="links" to={`/projetos/`}>
               <p >Projetos</p>
             </Link>
-            <Link className="links" to={`/Conhecimentos/`}>
+              {/* @ts-ignore */}
+            <Link className="links" to={`/conhecimentos/`}>
               <p >Conhecimentos</p>
             </Link>
-            <Link className="links" to={`/Contatos/`}>
+              {/* @ts-ignore */}
+            <Link className="links" to={`/contatos/`}>
               <p >Contatos</p>
             </Link>
             <BurguerMenu id="burguer-menu"/>
