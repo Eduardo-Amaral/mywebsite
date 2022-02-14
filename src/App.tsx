111488e22c9581b2ddfbc3adac01accdfe634445
Eduardo-Amaral <eduardo.lopes.amaral@outlook.com>
2022-02-14 08:00:57 -0300

    fixed bug in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const AnimatedSwitch = () => {
   useEffect(() => {
     if (transitionName === "next") setTransitionName("prev");
     if (transitionName === "prev") setTransitionName("next");
-  }, [location]);
+  }, [transitionName]);
   
 
   return (
